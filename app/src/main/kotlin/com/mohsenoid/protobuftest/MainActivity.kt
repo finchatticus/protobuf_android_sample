58ca4797f93f681a740ee7daeea552b334d9d7ad
vlad <kozii.vladyslav@gmail.com>
2018-06-07 16:45:42 +0300

    Add comparing JSON vs Gzip JSON vs protocol buffers

--- app/src/main/kotlin/com/mohsenoid/protobuftest/MainActivity.kt
+++ app/src/main/kotlin/com/mohsenoid/protobuftest/MainActivity.kt
@@ -2,79 +2,65 @@ package com.mohsenoid.protobuftest
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
+import org.json.JSONObject
+import java.io.ByteArrayOutputStream
+import java.io.IOException
+import java.util.zip.GZIPOutputStream
 
 class MainActivity : AppCompatActivity() {
+
+    private companion object {
+        val TAG = MainActivity::class.java.simpleName
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val addressBook = createAddressBookProtobuf()
-
-        // Serialize AddressBook to ByteArray
-        val bytes = addressBook.toByteArray()
-
-        // Deserialize AddressBook ByteArray
-        val myAddressBook = AddressBookProtos.AddressBook.parseFrom(bytes)
-        println(myAddressBook)
-
-        println("Protobuf byte size: ${bytes.size}")
-        println("JSON byte size: ${SAMPLE_JSON.toByteArray().size}")
-    }
-
-    private fun createAddressBookProtobuf(): AddressBookProtos.AddressBook {
-        // building PhoneNumber objects
-        val phoneHome = AddressBookProtos.Person.PhoneNumber.newBuilder()
-                .setNumber("+49123456")
-                .setType(AddressBookProtos.Person.PhoneType.HOME)
-                .build()
-        val phoneMobile = AddressBookProtos.Person.PhoneNumber.newBuilder()
-                .setNumber("+49654321")
-                .setType(AddressBookProtos.Person.PhoneType.MOBILE)
-                .build()
-
-        // building a Person object using phone data
-        val person = AddressBookProtos.Person.newBuilder()
-                .setId(1)
-                .setName("Mohsen")
-                .setEmail("info@mohsenoid.com")
-                .addAllPhones(listOf(phoneHome, phoneMobile))
-                .build()
-
-        // building an AddressBook object using person data
-        return AddressBookProtos.AddressBook.newBuilder()
-                .addAllPeople(listOf(person))
-                .build()
-    }
-
-    companion object {
-        private const val SAMPLE_JSON =
-                """{
-  "addressbook": [
-    {
-      "person": {
-        "id": 1,
-        "name": "Mohsen",
-        "email": "info@mohsenoid.com"
-      }
-    },
-    {
-      "phones": [
-        {
-          "phone": {
-            "number": "+49123456",
-            "type": "HOME"
-          }
-        },
-        {
-          "phone": {
-            "number": "+49654321",
-            "type": "MOBILE"
-          }
+        val jsonString = resources.getString(R.string.data_set)
+        val ecgJson = JSONObject(jsonString)
+        val dateStart = ecgJson.getInt("dateStart")
+        val dateEnd = ecgJson.getInt("dateEnd")
+        val frequency = ecgJson.getInt("frequency")
+        val dataSet = ecgJson.getJSONArray("dataSet")
+        val dataSetList = mutableListOf<Int>()
+        for (i in 0 until dataSet.length()) {
+            dataSetList.add(dataSet.getInt(i))
         }
-      ]
+
+        val ecg = EcgProtos.Ecg.newBuilder()
+                .setDateStart(dateStart)
+                .setDateEnd(dateEnd)
+                .setFrequency(frequency)
+                .addAllDataSet(dataSetList)
+                .build()
+
+        // Serialize Ecg to ByteArray
+        val bytes = ecg.toByteArray()
+
+        // Compress JSON by Gzip
+        val compressed = compress(jsonString)
+
+        val jsonSize = jsonString.toByteArray().size
+        val compressedSize = compressed.size
+        val protoBufSize = bytes.size
+        Log.d(TAG, "JSON      size: $jsonSize byte, ${jsonSize.toKb()} kB")
+        Log.d(TAG, "Gzip JSON size: $compressedSize byte, ${compressedSize.toKb()} kB")
+        Log.d(TAG, "Protobuf  size: $protoBufSize byte, ${protoBufSize.toKb()} kB")
     }
-  ]
-}"""
+
+    private fun Int.toKb() = this / 1024
+
+    @Throws(IOException::class)
+    fun compress(string: String): ByteArray {
+        val os = ByteArrayOutputStream(string.length)
+        val gos = GZIPOutputStream(os)
+        gos.write(string.toByteArray())
+        gos.close()
+        val compressed = os.toByteArray()
+        os.close()
+        return compressed
     }
+
 }
